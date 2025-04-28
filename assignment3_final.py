dc8f5c9661b24575b040f621f7b4bba5742c0242
Tarunno360 <sm.azmain.faysal@g.bracu.ac.bd>
2025-04-29 01:08:43 +0600

    reset game logic

--- assignment3_final.py
+++ assignment3_final.py
@@ -14,6 +14,10 @@ game_stopped = False
 #player all variables 
 player_current_position = [0, 0, 0]
 player_current_angle = 0
+player_health_bar=5
+
+#game logic
+score_card=0
 
 #camera all variables
 current_camera_position = [0, -500, 300]
@@ -26,6 +30,10 @@ central_position_grid=(number_of_tiles*number_of_grids)/2
 
 total_enemy_at_a_time=5
 enemies_char=[]
+
+#bullet variables
+bullet_char=[]
+total_missed_bullets=0
 
 def draw_text(x, y, text, font=GLUT_BITMAP_HELVETICA_12):
     glColor3f(1, 1, 1)
@@ -115,8 +123,58 @@ def draw_player_shape():
 
     glPopMatrix()
 
-
+def draw_enemy_shape():
+    if game_stopped==True:
+        return
+    for each_enemy in enemies_char:
+        glPushMatrix()
+        glTranslatef(each_enemy[0], each_enemy[1], each_enemy[2]+15)
+        beep=each_enemy[3]
+        glScalef(beep, beep, beep)
+        glColor3f(1, 0, 0)
+        glutSolidSphere(15,20,20)
         
+        glPushMatrix()
+        glTranslatef(0, 0, 20)
+        glColor3f(0, 0, 0)
+        glutSolidSphere(10, 20, 20)
+        glPopMatrix()
+        
+        glPopMatrix()
+        
+def draw_bullet_pallents():
+    if game_stopped==True:
+        return
+    glColor3f(1, 0, 0)
+    for each_bullet in bullet_char:
+        glPushMatrix()
+        glTranslatef(each_bullet["position"][0], each_bullet["position"][1], 65)
+        glutSolidSphere(5, 15, 15)
+        glPopMatrix()
+
+def reset_game():
+    global player_current_position, player_current_angle, current_camera_position, game_stopped,player_health_bar, score_card, total_missed_bullets
+    global enemies_char, bullet_char
+    
+    player_current_position = [0, 0, 0]
+    player_current_angle = 0
+    current_camera_position = [0, -500, 300]
+    game_stopped = False
+    
+    enemies_char.clear()
+    bullet_char.clear()
+    score_card=0
+    total_missed_bullets=0
+    player_health_bar=5
+    for _ in range(total_enemy_at_a_time):
+        temp = create_enemy_position()
+        enemy_temp = temp + [1.0, 1] 
+        enemies_char.append(enemy_temp)
+        
+    
+
+    
+    
     
 
     
